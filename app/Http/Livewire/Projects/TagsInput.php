3ef20931925ecd107a1ef8706b1315ad9e2d5ca3
Sam Brands <d286050@edu.rocwb.nl>
2023-01-14 15:53:23 +0100

    Livewire now passes tags to view

--- app/Http/Livewire/Projects/TagsInput.php
+++ app/Http/Livewire/Projects/TagsInput.php
@@ -3,9 +3,27 @@
 namespace App\Http\Livewire\Projects;
 
 use Livewire\Component;
+use Spatie\Tags\Tag;
 
 class TagsInput extends Component
 {
+    public $projectTags = [];
+
+    public function mount($project = [])
+    {
+        if ($project) {
+            $this->projectTags = $this->formatTags($project->tags);
+        }
+        $this->allTags = $this->formatTags(Tag::all());
+    }
+    
+    public function formatTags($tags)
+    {
+        return $tags->map(function ($tag) {
+            return ['id' => $tag->id, 'text' => $tag->name];
+        })->toArray();
+    }
+    
     public function render()
     {
         return view('livewire.projects.tags-input');
