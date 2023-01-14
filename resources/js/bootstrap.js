d29c35e6084efa1777940d668fe2f3cf5df9e4e0
Sam Brands <d286050@edu.rocwb.nl>
2023-01-14 15:45:19 +0100

    Installed dependencies for new tag input

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -10,6 +10,12 @@ window._ = _;
 import axios from 'axios';
 window.axios = axios;
 
+import jQuery from 'jquery';
+window.$ = jQuery;
+
+import select2 from 'select2';
+window.select2 = select2();
+
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
