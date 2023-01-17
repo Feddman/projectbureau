fa73bfef7a5b7e8010f853bb536472e4460e2fa0
TjKalau <71122246+TjKalau@users.noreply.github.com>
2023-01-17 11:18:29 +0100

    Map + index.blade for messages

---
+++ resources/views/messages/index.blade.php
@@ -0,0 +1,15 @@
+<x-app-layout>
+    <x-slot name="header">
+        <H1>Messages</H1>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
