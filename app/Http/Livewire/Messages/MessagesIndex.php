8beb2fed96709f9e149192ee1880a7be20bbf28a
TjKalau <71122246+TjKalau@users.noreply.github.com>
2023-01-17 11:38:00 +0100

    Created livewire for messages

---
+++ app/Http/Livewire/Messages/MessagesIndex.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class MessagesIndex extends Component
+{
+    public function render()
+    {
+        return view('livewire.messages-index');
+    }
+}
