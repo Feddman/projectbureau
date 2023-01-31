812b7e903047afe4ee3e31640491c9633f4d7885
Inetjuhhh <35923627+Inetjuhhh@users.noreply.github.com>
2023-01-31 15:52:39 +0100

    added correct color and html for button

--- resources/views/components/project-card.blade.php
+++ resources/views/components/project-card.blade.php
@@ -1,10 +1,12 @@
-<div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md @if($project->groups->count() == 0) bg-lime-200 @else bg-orange-200 @endif">
-    <a href="#">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">{{$project->name}}</h5>
-        <p class="font-bold"><i>type: {{$project->type->name}}</i></p>
-    </a>
-    <p class="mb-3 font-normal text-gray-700">{{ $project->description }}</p>
-    <a href="{{route('projects.show', $project)}}" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-yellow-700 rounded-lg hover:bg-yellow-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
+<div class="flex flex-col justify-between max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md @if($project->groups->count() == 0) bg-lime-200 @else bg-orange-200 @endif">
+    <div class="flex-col">
+        <a href="#">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">{{$project->name}}</h5>
+            <p class="font-bold"><i>type: {{$project->type->name}}</i></p>
+        </a>
+        <p class="mb-3 font-normal text-gray-700">{{ $project->description }}</p>
+    </div>
+    <a href="{{route('projects.show', $project)}}" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-yellow-700 rounded-lg hover:bg-yellow-800 focus:ring-4 focus:outline-none focus:ring-blue-300 @if($project->groups->count() == 0) bg-lime-700 hover:bg-lime-800 @else bg-orange-700 @endif">
         Naar projectpagina
         <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
     </a>
