375c33190325f8cfdee559072d6d35747a9fa41b
Sam Brands <d286050@edu.rocwb.nl>
2023-02-03 14:55:55 +0100

    Removed unread indicator from navigation
    Just commented it out so it can be used in the future.

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -44,9 +44,11 @@
                 
                 <a href="{{route('messages.index')}}" type="button" class="inline-flex items-center text-sm font-medium text-center text-gray-500 hover:text-gray-900 focus:outline-none">
                     <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z"></path></svg>
-                    <div class="relative flex">
-                        <div class="relative inline-flex w-3 h-3 bg-red-500 border-2 border-white rounded-full -top-2 right-3"></div>
-                    </div>
+	                {{-- Block of code below is for an indicator when the user has unread messages. --}}
+
+	                {{-- <div class="relative flex"> --}}
+	                {{-- <div class="relative inline-flex w-3 h-3 bg-red-500 border-2 border-white rounded-full -top-2 right-3"></div> --}}
+	                {{-- </div> --}}
                 </a>
 
                 <x-dropdown align="right" width="48">
