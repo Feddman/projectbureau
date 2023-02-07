4e0d37d7f3f801d8edca55e2231de4c282e000be
Sam Brands <d286050@edu.rocwb.nl>
2023-02-07 14:33:48 +0100

    Added button for creating a message
    Only visible to users with 'Send Messages' permission

--- resources/views/livewire/messages/messages-index.blade.php
+++ resources/views/livewire/messages/messages-index.blade.php
@@ -1,55 +1,69 @@
-<div class="flex flex-row w-full">
-	<div class="basis-1/2">
+<div>
+	<div class="p-4 flex justify-between items-center content-start">
+		@can('Send Messages')
+			<h2 class="my-2 border-bottom-2 text-2xl">Alle berichten</h2>
 
-		<div id="accordion-color" class="group dark:text-gray-500" data-accordion="collapse"
-		     data-active-classes="bg-blue-100 text-blue-600 !rounded-b-none">
-			@forelse ($groups as $group)
+			<a href="{{ route('messages.create') }}"
+			   class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">+ Nieuw bericht
+			</a>
+		@else
+			<h2 class="my-2 border-bottom-2 text-2xl">Mijn berichten</h2>
+		@endcan
+	</div>
 
-				<div class="group/item" wire:key="group-{{$group->id}}">
-					<h2 id="accordion-color-heading-{{$loop->index + 1}}">
-						<button wire:click="selectGroup({{$group}})" type="button"
-						        class="group/button group-first/item:rounded-t-xl group-last/item:rounded-b-xl flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-gray-200 focus:ring-4 focus:ring-blue-200 hover:bg-blue-100 @if($this->checkGroupAlreadySelected($group)) bg-blue-100 text-blue-600 !rounded-b-none @endif ">
+	<div class="flex flex-row w-full">
+		<div class="basis-1/2">
 
-							<span>{{$group->name}}</span>
+			<div id="accordion-color" class="group" data-accordion="collapse"
+			     data-active-classes="bg-blue-100 text-blue-600 !rounded-b-none">
 
-							<svg data-accordion-icon
-							     class="w-6 h-6 @if($this->checkGroupAlreadySelected($group)) rotate-180 @endif shrink-0"
-							     fill="currentColor"
-							     viewBox="0 0 20 20"
-							     xmlns="http://www.w3.org/2000/svg">
-								<path fill-rule="evenodd"
-								      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-								      clip-rule="evenodd"></path>
-							</svg>
-						</button>
-					</h2>
+				@forelse ($groups as $group)
+					<div class="group/item" wire:key="group-{{$group->id}}">
+						<h2 id="accordion-color-heading-{{$loop->index + 1}}">
+							<button wire:click="selectGroup({{$group}})" type="button"
+							        class="group/button group-first/item:rounded-t-xl group-last/item:rounded-b-xl flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-gray-200 focus:ring-4 focus:ring-blue-200 hover:bg-blue-100 @if($this->checkGroupAlreadySelected($group)) bg-blue-100 text-blue-600 !rounded-b-none @endif ">
 
-					<div id="accordion-color-body-{{$loop->index + 1}}"
-					     class="group/content @if(!$this->checkGroupAlreadySelected($group)) hidden @endif">
-						<div class="group-last/item:rounded-b-xl font-light border border-gray-200">
+								<span>{{$group->name}}</span>
 
-							<ul class="w-full text-sm font-medium text-gray-900">
-								@forelse($group->messages as $message)
-									<x-message-li wire:click="showMessage({{$message}})"
-									              wire:key="message-{{$message->id}}" :message="$message"
-									              :selectedMessage="$selectedMessage"/>
-								@empty
-									<div class="text-center py-3">
-										<p>Nog geen berichten</p>
-									</div>
-								@endforelse
-							</ul>
+								<svg data-accordion-icon
+								     class="w-6 h-6 @if($this->checkGroupAlreadySelected($group)) rotate-180 @endif shrink-0"
+								     fill="currentColor"
+								     viewBox="0 0 20 20"
+								     xmlns="http://www.w3.org/2000/svg">
+									<path fill-rule="evenodd"
+									      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+									      clip-rule="evenodd"></path>
+								</svg>
+							</button>
+						</h2>
+
+						<div id="accordion-color-body-{{$loop->index + 1}}"
+						     class="group/content @if(!$this->checkGroupAlreadySelected($group)) hidden @endif">
+							<div class="group-last/item:rounded-b-xl font-light border border-gray-200">
+								<ul class="w-full text-sm font-medium text-gray-900">
+									@forelse($group->messages as $message)
+										<x-message-li wire:click="showMessage({{$message}})"
+										              wire:key="message-{{$message->id}}" :message="$message"
+										              :selectedMessage="$selectedMessage"/>
+									@empty
+										<div class="text-center py-3">
+											<p>Nog geen berichten</p>
+										</div>
+									@endforelse
+								</ul>
+							</div>
 						</div>
 					</div>
-				</div>
 
-			@empty
-				<div class="text-center py-3">
-					<p>Nog geen groepen</p>
-				</div>
-			@endforelse
+				@empty
+
+					<div class="text-center py-3">
+						<p>Nog geen groepen</p>
+					</div>
+				@endforelse
+
+			</div>
 		</div>
-	</div>
 
 	<div class="basis-1/2">
 		<div class="px-5">
