9c36fdbd889f555e3a33e395f1982c3a890b1c88
Inetjuhhh <35923627+Inetjuhhh@users.noreply.github.com>
2023-02-08 17:41:44 +0100

    change didn't work to resolve conflict groupController. So change it back

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -19,7 +19,7 @@ class GroupController extends Controller
      */
     public function index()
     {
-        $groups = Group::paginate(6)->sortBy('created_at');
+        $groups = Group::all()->sortBy('created_at');
         return view('groups.index', ['groups' => $groups]);
     }
 
