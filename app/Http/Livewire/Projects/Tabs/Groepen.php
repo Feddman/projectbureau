5edc3457a4d05196b7d4c6cf822597e0902c2c6c
tahar2004 <taharsoldaat@gmail.com>
2023-02-16 11:23:28 +0100

    E-mail
    als een groep gekoppled wordt aan een project krijg iedereen in de groep en e-mail van dat ze gekoppeld zijn

--- app/Http/Livewire/Projects/Tabs/Groepen.php
+++ app/Http/Livewire/Projects/Tabs/Groepen.php
@@ -3,7 +3,13 @@
 namespace App\Http\Livewire\Projects\Tabs;
 
 use App\Models\Group;
+use App\Mail\meldingSend;
+use App\Models\GroupUser;
+use App\Models\Project;
+use Illuminate\Support\Facades\Mail;
 use Livewire\Component;
+use App\Models\User;
+use Illuminate\Support\Facades\URL;
 
 class Groepen extends Component
 {
@@ -21,8 +27,33 @@ class Groepen extends Component
 
     public function saveGroup() {
         $group = Group::find($this->groupSelected);
-        $this->project->groups()->attach($group);
-        $this->project->groups = $this->project->groups()->get();
+
+        $groupusers = GroupUser::all()->where('group_id', $group->id);
+
+        $url = URL::previous();
+        $parts = Explode('/', $url);
+        $id = $parts[count($parts) - 1];
+
+        $project = Project::where('id',$id)->first();
+
+        foreach($groupusers as $groupuser){
+            if($groupuser->user_id != null){
+                $user = User::all()->where('id', $groupuser->user_id)->first();
+
+                session()->put('projectnaam', $project->name);
+                session()->put('groepnaam', $group->name);
+                session()->put('username', $user->name);
+                session()->put('useremail', $user->email);
+
+                $Email = $user->email;
+
+                Mail::to($Email)->send(new meldingSend);
+            }
+        }
+
+        // $this->project->groups()->attach($group);
+        // $this->project->groups = $this->project->groups()->get();
+
     }
 
     public function render()
